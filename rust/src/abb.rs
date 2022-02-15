a37b3f75bb98573d4eea75cd1df248b31ca4513f
ogty <mofuiti1803@gmail.com>
2022-02-15 12:20:36 +0900

    add

---
+++ rust/src/abb.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+
+pub trait Abb<T> {
+    fn abb(&mut self, c:T) -> String ;
+}
+pub struct AbbImpl {
+    month_word: Vec<String>,
+    month_num: Vec<i32>,
+}
+
+impl Abb<i32> for AbbImpl {
+    fn abb(&mut self, c:i32) -> String {
+        let months: HashMap<_, _> = self.month_num.iter().zip(self.month_word.iter()).collect();
+        let month = months.get(&c);
+        let result = format!("{:?}", month);
+        return result
+    }
+}
+
+impl Abb<String> for AbbImpl {
+    fn abb(&mut self, c:String) -> String {
+        let months: HashMap<_, _> = self.month_word.iter().zip(self.month_num.iter()).collect();
+        let month = months.get(&c);
+        let result = format!("{:?}", month);
+        return result
+    }
+}
+
+pub fn abb<T>(data:T) -> String where AbbImpl:Abb<T> {
+    let month_word = vec![
+        String::from("Jan"), String::from("Feb"), String::from("Mar"),
+        String::from("Apr"), String::from("May"), String::from("Jun"),
+        String::from("Jul"), String::from("Aug"), String::from("Sep"),
+        String::from("Oct"), String::from("Nov"), String::from("Dec")
+    ];
+    let month_num = (1..13).collect::<Vec<i32>>();
+    let mut abb = AbbImpl{month_word, month_num};
+    let mut result = Abb::abb(&mut abb, data);
+    result = result.replace("Some(", "").replace("\"", "");
+    result = result.replace(")", "");
+    return result
+}
---
+++ rust/src/main.rs
@@ -0,0 +1,17 @@
+mod abb;
+
+fn main() {}
+
+#[test]
+fn test_is_abb_ok() {
+    assert_eq!(abb::abb(10), "Oct");
+    assert_eq!(abb::abb(String::from("Feb")), "2".to_string());
+    assert_eq!(abb::abb(String::from("Jun")).parse::<i32>().unwrap(), 6);
+}
+
+#[test]
+fn test_is_abb_no() {
+    assert_ne!(abb::abb(10), "Dec");
+    assert_ne!(abb::abb(String::from("Feb")), "6".to_string());
+    assert_ne!(abb::abb(String::from("Jun")).parse::<i32>().unwrap(), 8);
+}
